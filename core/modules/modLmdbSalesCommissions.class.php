55937eac7e3ee45388fce8d094060a44f0e3eb46
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-07-06 16:46:55 +0200

    Colore le pictogramme du module
    Ajoute la couleur jaune au pictogramme fa-percent du descripteur de module avec la syntaxe native Dolibarr, pour que l’icône apparaisse en jaune dans la liste des modules.

--- core/modules/modLmdbSalesCommissions.class.php
+++ core/modules/modLmdbSalesCommissions.class.php
@@ -39,7 +39,7 @@ class modLmdbSalesCommissions extends DolibarrModules
 		$this->descriptionlong = 'LmdbSalesCommissionsDescLong';
 		$this->version = '0.1.0';
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
-		$this->picto = 'fa-percent';
+		$this->picto = 'fa-percent_fas_#f0b400';
 		$this->editor_name = 'Pierre Ardoin';
 		$this->editor_url = '';
 
